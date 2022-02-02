9cfb7e4e60217da233bb572ed88950300c222157
Emerald <theemeraldseason@icloud.com>
2022-02-02 19:50:55 +1000

    rege

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,44 +1,15 @@
 const Discord = require('discord.js');
-
-module.exports = {
-    name: 'ban',
-    aliases: [],
-    utilisation: '{prefix}ban',
-
-    async run (client, message, args) {
-
-        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can't use that!')
-        if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don't have the right permissions.')
-
-        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-
-        if(!args[0]) return message.channel.send('Please specify a user');
-
-        if(!member) return message.channel.send('Can't seem to find this user. Sorry 'bout that :/');
-        if(!member.bannable) return message.channel.send('This user can't be banned. It is either because they are a mod/admin, or their highest role is higher than mine');
-
-        if(member.id === message.author.id) return message.channel.send('Bruh, you can't ban yourself!');
-
-        let reason = args.slice(1).join(" ");
-
-        if(!reason) reason = 'Unspecified';
-
-        member.ban(`${reason}`).catch(err => { 
-          message.channel.send('Something went wrong')
-            console.log(err)
-        })
-
-        const banembed = new Discord.MessageEmbed()
-        .setTitle('Member Banned')
-        .setThumbnail(member.user.displayAvatarURL())
-        .addField('User Banned', member)
-        .addField('Kicked by', message.author)
-        .addField('Reason', reason)
-        .setFooter('Time kicked', client.user.displayAvatarURL())
-        .setTimestamp()
-
-        message.channel.send(banembed);
-
-
-    }
-}
+    module.exports = {
+        minArgs: 1,
+        requiredPermissions: ["ADMINISTRATOR", "BAN_MEMBERS"],
+        syntaxError: "Incorrect syntax, please use !ban <member>",
+        callback: ({ message }) => {
+          const { member, mentions } = message;
+          const target = mentions.users.first();
+          if (target) {
+            const targetMember = message.guild.members.cache.get(target.id);
+            targetMember.ban();
+            message.channel.send(`<@${member.id}> That user has been banned`);
+          }
+        },
+      };
