7fbae1987d690e36f07703108446da049c01d86a
Emerald <theemeraldseason@icloud.com>
2022-02-07 18:14:24 +1000

    Help

--- commands/informational/help.js
+++ commands/informational/help.js
@@ -10,16 +10,19 @@ module.exports = {
         .setTitle('title')
         .setColor('RANDOM')
         .addField('line 1')
+        .addField('line 2')
 
         const second = new MessageEmbed()
         .setTitle('title')
         .setColor('RANDOM')
         .addField('line1')
+        .addField('line2')
 
         const third = new MessageEmbed()
         .setTitle('title')
         .setColor('RANDOM')
         .addField('line1')
+        .addField('line2')
 
 
         const pages = [first, second, third];
