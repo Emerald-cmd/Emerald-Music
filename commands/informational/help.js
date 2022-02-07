71c6869e27a4a60f4711828143a3cb0b5292bd09
Emerald <theemeraldseason@icloud.com>
2022-02-07 19:47:30 +1000

    wefwef

--- commands/informational/help.js
+++ commands/informational/help.js
@@ -9,19 +9,14 @@ module.exports = {
     execute(client, message, args) {
         const embed = new MessageEmbed();
         embed.setColor('#0099ff')
-        embed.setTitle('Some title')
-        embed.setURL('https://discord.js.org/')
-        embed.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-        embed.setDescription('Some description here')
+        embed.setTitle('List of Commands 📃')
+        embed.setDescription('Listed are commands any players can use!')
         embed.setThumbnail('https://i.imgur.com/AfFp7pu.png')
         embed.addFields(
-            { name: 'Regular field title', value: 'Some value here' },
-            { name: '\u200B', value: '\u200B' },
             { name: 'Inline field title', value: 'Some value here', inline: true },
             { name: 'Inline field title', value: 'Some value here', inline: true },
         )
         embed.addField('Inline field title', 'Some value here', true)
-        embed.setImage('https://i.imgur.com/AfFp7pu.png')
         embed.setTimestamp()
         embed.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
         message.channel.send({ embeds: [embed] });
