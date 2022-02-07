d94e55ee5c236f09dbe7f46d0fafef57d70ae580
Emerald <theemeraldseason@icloud.com>
2022-02-07 17:26:41 +1000

    wefwe

--- commands/informational/help.js
+++ commands/informational/help.js
@@ -1,18 +1,26 @@
 const { MessageEmbed } = require('discord.js');
+const { ReactionPages } = require('reconlx')
 
 module.exports = {
     name: 'help',
-    aliases: ['h'],
-    showHelp: false,
-    utilisation: '{prefix}help',
+    aliases: [],
+    utilisation: '{prefix}helpline',
+    execute(client, message, args, Discord){
+        const first = new MessageEmbed()
+        .setTitle('Need Help?')
+        .setColor('GREEN')
+        .addField('Prefix: `.`')
+        .addField('Who made me?', 'emerald')
+        .addField('Commands', 'Page 2 : Normal Commands', 'Page 3 : Music Commands')
 
-    execute(client, message, args) {
-        const embed = new MessageEmbed();
-		embed.setTitle("Command List 📃")
-		embed.setDescription("`.help` : Pulls this list of commands up *(h)*\n\n\`.help-music` : Pulls up help list for Music *(hm, music)*\n\n\`.ping` : Shows the ping of the bot *(latency)*\n\n\`.server` : Shows the server stats *(server-info)*\n\n\`.user` : Shows your user info *(user-info)*\n\n\`.helpline` : Messages you a list of help phone hotlines | *TW*")
-		embed.setColor("#00D166")
-        embed.setTimestamp();
-        embed.setFooter('Emerald Bot - by emerald#0652', message.author.avatarURL({ dynamic: true }));
-        message.channel.send({ embeds: [embed] });
-    },
-};
+        const second = new MessageEmbed()
+        .setTitle('Commands')
+        .setColor('GREEN')
+        .addField('Prefix: `.`')
+        .addField('.help')
+        .addField('a')
+
+        const pages = [first, second];
+        ReactionPages(message, pages, false)
+    }
+}
