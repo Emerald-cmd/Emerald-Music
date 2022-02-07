439cd6fc5124d2e178d39c679198876738fa734d
Emerald <theemeraldseason@icloud.com>
2022-02-07 19:43:52 +1000

    fw

--- commands/informational/help.js
+++ commands/informational/help.js
@@ -1,67 +1,29 @@
 const { MessageEmbed } = require('discord.js');
-const { ReactionPages } = require('reconlx')
 
 module.exports = {
     name: 'help',
-    aliases: [],
+    aliases: ['h'],
+    showHelp: false,
     utilisation: '{prefix}help',
-    execute(client, message, args, Discord){
-        const first = new MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle('Some title')
-        .setURL('https://discord.js.org/')
-        .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-        .setDescription('Some description here')
-        .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-        .addFields(
+
+    execute(client, message, args) {
+        const embed = new MessageEmbed();
+        embed.setColor('#0099ff')
+        embed.setTitle('Some title')
+        embed.setURL('https://discord.js.org/')
+        embed.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+        embed.setDescription('Some description here')
+        embed.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+        embed.addFields(
             { name: 'Regular field title', value: 'Some value here' },
             { name: '\u200B', value: '\u200B' },
             { name: 'Inline field title', value: 'Some value here', inline: true },
             { name: 'Inline field title', value: 'Some value here', inline: true },
         )
-        .addField('Inline field title', 'Some value here', true)
-        .setImage('https://i.imgur.com/AfFp7pu.png')
-        .setTimestamp()
-        .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-
-        const second = new MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle('Some title')
-        .setURL('https://discord.js.org/')
-        .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-        .setDescription('Some description here')
-        .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-        .addFields(
-            { name: 'Regular field title', value: 'Some value here' },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-        )
-        .addField('Inline field title', 'Some value here', true)
-        .setImage('https://i.imgur.com/AfFp7pu.png')
-        .setTimestamp()
-        .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-        
-        const third = new MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle('Some title')
-        .setURL('https://discord.js.org/')
-        .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-        .setDescription('Some description here')
-        .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-        .addFields(
-            { name: 'Regular field title', value: 'Some value here' },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-        )
-        .addField('Inline field title', 'Some value here', true)
-        .setImage('https://i.imgur.com/AfFp7pu.png')
-        .setTimestamp()
-        .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-
-
-        const pages = [first, second, third];
-        ReactionPages(message, pages, false)
-    }
-}
+        embed.addField('Inline field title', 'Some value here', true)
+        embed.setImage('https://i.imgur.com/AfFp7pu.png')
+        embed.setTimestamp()
+        embed.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+        message.channel.send({ embeds: [embed] });
+    },
+};
