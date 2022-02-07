ff010331a5385570ad905597b5ce15a73f7e77f9
Emerald <theemeraldseason@icloud.com>
2022-02-07 19:51:51 +1000

    rgrgrgr

--- commands/informational/help.js
+++ commands/informational/help.js
@@ -16,14 +16,9 @@ module.exports = {
             { name: '\u200B', value: '\u200B' },
             { name: 'Inline field title', value: 'Some value here', inline: true },
             { name: 'Inline field title', value: 'Some value here', inline: true },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
+            { name: '\nInline field title', value: '\nSome value here', inline: true },
         )
-        embed.addFields(
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-        )
+        
         embed.setTimestamp()
         embed.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
         message.channel.send({ embeds: [embed] });
