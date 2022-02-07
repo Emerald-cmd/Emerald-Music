29e753dee2f5cb546a1ae15c203c5415d43ca917
Emerald <theemeraldseason@icloud.com>
2022-02-07 17:42:55 +1000

    Help

--- commands/informational/help.js
+++ commands/informational/help.js
@@ -3,21 +3,25 @@ const { ReactionPages } = require('reconlx')
 
 module.exports = {
     name: 'help',
-    aliases: [],
-    utilisation: '{prefix}helpline',
+    description: 'this is a help command',
     execute(client, message, args, Discord){
         const first = new MessageEmbed()
-        .setTitle('Need Help?')
-        .setColor('GREEN')
-        .addField('Prefix:')
+        .setTitle('title')
+        .setColor('RANDOM')
+        .addField('line 1')
 
         const second = new MessageEmbed()
-        .setTitle('Commands')
-        .setColor('GREEN')
-        .addField('Prefix:')
+        .setTitle('title')
+        .setColor('RANDOM')
+        .addField('line1')
+
+        const third = new MessageEmbed()
+        .setTitle('title')
+        .setColor('RANDOM')
+        .addField('line1')
 
 
-        const pages = [first, second];
+        const pages = [first, second, third];
         ReactionPages(message, pages, false)
     }
 }
