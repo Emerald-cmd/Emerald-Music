9e3e7d8de2da5ab670f035961aab305c79dddab0
Emerald <theemeraldseason@icloud.com>
2022-02-16 14:21:24 +1000

    staff help

--- commands/moderation/staff-help.js
+++ commands/moderation/staff-help.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'staff-help',
-    aliases: ['staff'],
+    aliases: ['hs',"staff",'help-staff'],
     showHelp: false,
     utilisation: '{prefix}staff-help',
 
@@ -13,9 +13,12 @@ module.exports = {
         embed.setDescription('Listed are commands any staff members can use!')
         embed.setThumbnail('https://i.imgur.com/aJX9Y95.png')
         embed.addFields(
-            { name: '.ban', value: 'bans a member from the server', inline: true },
+            { name: '.ban', value: 'bans a player from the server', inline: true },
             { name: '.kick', value: 'kicks a player from the server', inline: true },
             { name: '.mute', value: 'mutes a player from the server', inline: true },
+            { name: '.unmute', value: 'unmutes a player from the server', inline: true },
+            { name: '.unban', value: 'unbans a player from the server', inline: true },
+            { name: '.timeout', value: 'timesout a player from the server', inline: true },
         )
         embed.setTimestamp()
         embed.setFooter({ text: 'Emerald Bot - by emerald#0652', iconURL: 'https://i.imgur.com/oAl3oc9.png' });
