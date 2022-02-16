d60e04124007a928288b2e9804ba11f0a2ec1b48
Emerald <theemeraldseason@icloud.com>
2022-02-16 18:36:36 +1000

    dc

--- commands/informational/user.js
+++ commands/informational/user.js
@@ -3,15 +3,24 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
     name: 'user',
     aliases: ['user-info'],
+    showHelp: false,
     utilisation: '{prefix}user',
 
     execute(client, message, args) {
         const embed = new MessageEmbed();
-		embed.setTitle("Server Info")
-		embed.setDescription(`Your Username: ${message.author.username}\n\nYour Nickname: ${message.member.nickname}\n\nYour ID: ${message.author.id}\n\nCreation Date: ${message.author.createdAt}\n\nJoined Server On: ${message.member.joinedAt}`)
-		embed.setColor("#00D166")
-        embed.setTimestamp();
-        embed.setFooter('Emerald Bot - by emerald#0652', message.author.avatarURL({ dynamic: true }));
+        embed.setColor('#0099ff')
+        embed.setTitle('User Info 📃')
+        embed.setDescription('Your User Information.')
+        embed.setThumbnail('https://i.imgur.com/aJX9Y95.png')
+        embed.addFields(
+            { name: 'Your Username', value: `${message.author.username}`, inline: true },
+            { name: 'Your Nickname', value: `${message.member.nickname}`, inline: true },
+            { name: 'Your ID', value: `${message.author.id}`, inline: true },
+            { name: 'Creation Date', value: `${message.author.createdAt}`, inline: true },
+            { name: 'Joined Server On', value: `${message.member.joinedAt}`, inline: true },
+        )
+        embed.setTimestamp()
+        embed.setFooter({ text: 'Emerald Bot - by emerald#0652', iconURL: 'https://i.imgur.com/oAl3oc9.png' });
         message.channel.send({ embeds: [embed] });
     },
 };
