1f249fa6a8e87a98720ac8f3b07c0d88278be543
Emerald <theemeraldseason@icloud.com>
2022-02-18 17:52:49 +1000

    a

--- commands/informational/bailey.js
+++ commands/informational/bailey.js
@@ -9,8 +9,8 @@ module.exports = {
     execute(client, message, args) {
         const embed = new MessageEmbed();
         embed.setColor('#0099ff')
-        embed.setTitle(`${this.aliases} EATS COCK`)
-        embed.setDescription(`${this.aliases} LIKE TO SUCK A LOT OF COCK`)
+        embed.setTitle(`${alias} EATS COCK`)
+        embed.setDescription(`${alias} LIKE TO SUCK A LOT OF COCK`)
         embed.setTimestamp()
         embed.setFooter({ text: 'Emerald Bot - by emerald#0652', iconURL: 'https://i.imgur.com/oAl3oc9.png' });
         message.channel.send({ embeds: [embed] });
